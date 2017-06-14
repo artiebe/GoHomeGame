92dd8b759ea9670803ede48ba370a76b46afd940
Artis <abswift@ABSWIFTs-MacBook-Pro.local>
2017-06-14 22:13:02 +0900

    Initial upload of GoHome game

---
+++ Assets/ConsolePrinter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrinter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		float location;
+		location = 5.0f; //Distance in meters
+		print ("Welcome to Go Home!");
+		print ("A game where you need to find your way back.");
+		print ("Location:");
+		print (location);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
