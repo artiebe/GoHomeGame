6be23a7968fc058ef90001f94a04b41cf3caa30e
Artis <abswift@ABSWIFTs-MacBook-Pro.local>
2017-06-16 22:37:33 +0900

    First commit

--- Assets/ConsolePrinter.cs
+++ Assets/ConsolePrinter.cs
@@ -6,17 +6,35 @@ public class ConsolePrinter : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		float location;
-		location = 5.0f; //Distance in meters
+		float location = 3.0f;
+		float homeLocation = 2.1f;
+		float distance = homeLocation - location;
 		print ("Welcome to Go Home!");
 		print ("A game where you need to find your way back.");
-		print ("Location:");
-		print (location);
-
+		print ("Distance:" + distance);
+		print (distance);
+		if (location > homeLocation) {
+			print ("Go back");
+		}
+		if (location < homeLocation) {
+			print ("Move on");
+		}
+		if(location == homeLocation) {
+			print ("I am at home.");
+		}
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () { 
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) 
+		{
+			print ("Left key pressed");
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) 
+		{
+			print ("Right key pressed");
+		}
+
+
 	}
 }
